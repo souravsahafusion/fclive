5f31c117597b40edf723c90acc2c7367e049e745
n0x3u5-fc <nakshatra@fusioncharts.com>
2016-08-24 14:48:25 +0530

    Got properties from model

--- app/scripts/controllers/property.js
+++ app/scripts/controllers/property.js
@@ -1,21 +1,34 @@
 'use strict';
 
 /**
-* @ngdoc function
-* @name fcLiveApp.controller:MainCtrl
-* @description
-* # MainCtrl
-* Controller of the fcLiveApp
-*/
-(function(){
+ * @ngdoc function
+ * @name fcLiveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fcLiveApp
+ */
+(function() {
 
-angular.module('fcLiveApp')
-	.controller('PropertyCtrl', function ($scope, $rootScope) {
+	angular.module('fcLiveApp').controller('PropertyCtrl', function($scope, $rootScope) {
 
-		$scope.$on('categorySelected', function(event, msg){
-			console.log(msg);
-		});	
+		var self = this;
+		$scope.$on('categorySelected', function(event, msg) {
+			console.log($rootScope.data.properties);
+			$scope.categoryList = $rootScope.data.properties;
+			$scope.categoryName = msg;
+			$scope.categoryTarget = "";
+			for (var i = 0, categoryListLength = $scope.categoryList.length; i < categoryListLength; i++) {
+			if ($scope.categoryList[i].category == $scope.categoryName) {
+			$scope.categoryTarget = $scope.categoryList[i].target;
+			$scope.propertyList = $scope.categoryList[i].values;
+			console.log($scope.propertyList);
+			for (var j = 0, propertyListLength = $scope.propertyList.length; j < propertyListLength; j++) {
 
+			}
+			}
+			}
+		});
 
 	});
+
 })();
