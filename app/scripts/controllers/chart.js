ca3b494e038cf14a0d87906fcf649c5c95ea76fe
sumit-fusioncharts <sumit@fusioncharts.com>
2016-08-24 21:02:06 +0530

    table module updates

--- app/scripts/controllers/chart.js
+++ app/scripts/controllers/chart.js
@@ -22,7 +22,5 @@ angular.module('fcLiveApp')
 		$scope.$on('propertyChanged', function(){
 			revenueChart.setJSONData(raw.dataSource);
 		});	
-
-
 	});
 })();
--- app/scripts/controllers/table.js
+++ app/scripts/controllers/table.js
@@ -1,17 +1,25 @@
 'use strict';
 
 /**
-* @ngdoc function
-* @name fcLiveApp.controller:MainCtrl
-* @description
-* # MainCtrl
-* Controller of the fcLiveApp
-*/
-(function(){
+ * @ngdoc function
+ * @name fcLiveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fcLiveApp
+ */
 
-angular.module('fcLiveApp')
-	.controller('TableCtrl', function () {
-	});
+(function() {
+    angular.module('fcLiveApp')
+        .controller('TableCtrl', function($scope, $rootScope) {
+            var self = this,
+                data = $rootScope.data.raw.dataSource.data;
 
-
+            self.getData = function() {
+                return data;
+            };
+            self.datajson = data;
+    		self.onChangeData = function(){
+				$rootScope.$broadcast('propertyChanged');
+			};
+        });
 })();
