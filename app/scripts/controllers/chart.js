17e292fc56085c7925dd0f0021c666740cc1c024
debayan3007 <debayan@fusioncharts.com>
2016-08-25 19:40:22 +0530

    drag and drop CSV

--- app/scripts/controllers/chart.js
+++ app/scripts/controllers/chart.js
@@ -7,26 +7,26 @@
  * # MainCtrl
  * Controller of the fcLiveApp
  */
-(function() {
+(function () {
 
-    angular.module('fcLiveApp')
-        .controller('ChartCtrl', function($scope, $rootScope) {
-            var self = this,
-                //json for fusioncharts
-                raw = $rootScope.data.raw,
-                revenueChart = {};
-            //update json on change
-            self.updateSource = function() {
-                    revenueChart.setJSONData(raw.dataSource);
-                }
-                //calling fusioncharts to render
+	angular.module('fcLiveApp')
+		.controller('ChartCtrl', function ($scope, $rootScope) {
+			var self = this,
+				//json for fusioncharts
+				raw = $rootScope.data.raw,
+				revenueChart = {};
+			//update json on change
+			self.updateSource = function () {
+				revenueChart.setJSONData(raw.dataSource);
+			}
+			//calling fusioncharts to render
 
-            FusionCharts.ready(function() {
-                revenueChart = new FusionCharts(raw).render();
-            });
-            //broadcast name propertyChanged
-            $scope.$on('propertyChanged', function() {
-                self.updateSource();
-            });
-        });
+			FusionCharts.ready(function () {
+				revenueChart = new FusionCharts(raw).render();
+			});
+			//broadcast name propertyChanged
+			$scope.$on('propertyChanged', function () {
+				self.updateSource();
+			});
+		});
 })();
